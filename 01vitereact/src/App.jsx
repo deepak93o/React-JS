88999a813fa0240e8f04c3768014cbd00a40e9b8
Deepak bhatt <157270970+deepak93o@users.noreply.github.com>
2025-04-27 02:59:07 +0000

    vite react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
-
+import NewF from "./new"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <>
+    <NewF />
     <h1>
       React with Vite first Page | Deepak
     </h1>
+    </>
   )
 }
 
---
+++ 01vitereact/src/new.jsx
@@ -0,0 +1,7 @@
+function NewF() {
+    return(
+        <h3>This is new File</h3>
+    )
+}
+
+export default NewF;
