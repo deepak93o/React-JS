c3c3a3e7ce2f167fab854a453de7fb8f49bb4821
Deepak bhatt <157270970+deepak93o@users.noreply.github.com>
2025-04-27 06:56:39 +0000

    02counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter] = useState(5)
+  // let counter = 5;
+
+  const addValue = () => {
+    // counter = counter + 1;
+    setCounter(counter + 1)
+  }
+  const removeValue = () =>{
+    if(counter > 0){
+      setCounter(counter - 1);
+    } else {
+      alert("Value Can't be less then zero");
+      
+    }
+  }
+  
+  return (
+    <>
+      <h1>React</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add Value</button>
+      <br />
+      <button
+      onClick={removeValue}>
+      Remove Value</button>
+    </>
+  )
+}
+
+export default App
