12f1f3fea4a7b88f614aaec853aafb0d44062175
Deepak bhatt <157270970+deepak93o@users.noreply.github.com>
2025-04-27 03:40:47 +0000

    Updates...

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import NewF from "./new"
 function App() {
-  const [count, setCount] = useState(0)
+  const username = "Deepak Bhatt"
 
   return (
     <>
     <NewF />
     <h1>
-      React with Vite first Page | Deepak
+      React with Vite first Page | {username}
     </h1>
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,38 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+  return(
+    <div>
+      <h1>Custom React !</h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank',
+
+//   },
+//   children: 'Click me to Visit Google'
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const user = "React JS"
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'click me to visit google',
+  user
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App/>
 )
